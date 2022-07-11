c2ec2fc438d70e1e9d95c77cf49536166c40abc9
wangchao <wangchao@zhiyuanhui.com>
2022-07-11 17:04:18 +0800

    python 图像数据读取模块抽取

---
+++ com/pipichao/dataset/__init__.py
@@ -0,0 +1,6 @@
+__all__=[
+    "handwritingdigist"
+]
+'''
+数据集
+'''
---
+++ com/pipichao/dataset/handwritingdigist.py
@@ -0,0 +1,56 @@
+'''
+手写数字
+'''
+
+import numpy as np
+import gzip
+import struct
+
+
+def read_train_set():
+    root_dir = "../../../../data/digit/"
+    train__input_path = "t10k-images-idx3-ubyte.gz"
+    train_target_path = "t10k-labels-idx1-ubyte.gz"
+    # 训练集数据
+    train_input_set = read_gzip_img(root_dir +train__input_path)
+    print("样本输入：\n", train_input_set.shape)
+    train_target_set = read_gzip_lable(root_dir +train_target_path)
+    print("样本目标：\n", train_target_set.shape)
+    return (train_input_set,train_target_set)
+
+def read_test_set():
+    root_dir = "../../../../data/digit/"
+    test_input_path = "train-images-idx3-ubyte.gz"
+    test_target_path = "train-labels-idx1-ubyte.gz"
+    # 训练集数据
+    test_input_set = read_gzip_img(root_dir + test_input_path)
+    print("测试输入：\n", test_input_set.shape)
+    test_target_set = read_gzip_lable(root_dir + test_target_path)
+    print("测试目标：\n", test_target_set.shape)
+    return (test_input_set, test_target_set)
+
+
+
+def read_gzip_img(file_path):
+    gzip_file = gzip.GzipFile(file_path)
+    data = gzip_file.read()
+    magic, num, rows, columns, = struct.unpack(">iiii", data[:16])
+    dimession = rows * columns
+    X = np.zeros((num, rows, columns), dtype="uint8")
+    # print(X.shape)
+    offset = 16
+    for i in range(num):
+        a = np.frombuffer(data, dtype=np.int8, count=dimession, offset=offset)
+        X[i] = a.reshape((rows, columns))
+        offset = offset + dimession
+    print(X[1].shape)
+    return X
+
+
+def read_gzip_lable(file_path):
+    gzip_file = gzip.GzipFile( file_path)
+    data = gzip_file.read()
+    magic, num = struct.unpack(">ii", data[:8])
+    target = np.frombuffer(data, dtype=np.int8, count=num, offset=8)
+    # print(d)
+    return target
