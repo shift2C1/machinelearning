5248aa6a252e753bbecf4e40ba07571c39d0fb46
wangchao <wangchao@zhiyuanhui.com>
2022-08-25 17:14:55 +0800

    波士顿房价测试集拆分，使用线性回归训练模型，结果不太对，但是流程基本正确

--- com/pipichao/machinelearning/eg/trainmodel.py
+++ com/pipichao/machinelearning/eg/trainmodel.py
@@ -3,6 +3,9 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import numpy as np
 import  os
 
 
@@ -38,17 +41,29 @@ if __name__ == '__main__':
     print(prepared_data[0:3])
 
     train_set,test_set=train_test_split(prepared_data,test_size=0.2,random_state=42)
-    print("测试集")
+    print("训练集")
     print(train_set[0:3]) # numpy的切片 ：默认按照行取，第一维代表行索引，第二位代表列索引
     print(test_set.shape)
 
     train_target=train_set[:,8] #取第八列的 房价（median_house_value） 为目标值
-    print("测试集目标")
+    print("训练集目标")
     print(train_target[0:3])
     train_input=train_set[:,(0,1,2,3,4,5,6,7,9)] #取除了第8列之外的数据
-    print("测试集输入")
+    print("训练集输入")
     print(train_input[0:3])
 
+#     test 集
+    test_set_input=test_set[:,(0,1,2,3,4,5,6,7,9)]
+    test_set_target=test_set[:,8]
 
 
+# 使用线性回归先试一下
+    lr=LinearRegression()
+    lr.fit(train_input,train_target)
+    print("训练模型后")
+    output=lr.predict(test_set_input)
+    print("模型输出：",output[0:10])
+    print("实际目标：",test_set_target[0:10])
 
+    mse=mean_squared_error(test_set_target,output)
+    print("均方误差的开平发：",np.sqrt(mse))
